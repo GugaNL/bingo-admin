74af2962e127606cf2c03810c1f5e460c93c349c
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2022-10-27 10:30:22 -0300

    add comment

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container }  from './styles'
 
 
-const Button = ({children, ...rest }) => (
+const Button = ({children, ...rest }) => ( //O rest serve para funcionar o onClick
     <Container {...rest}>
         {children}
     </Container>
